c2cf3a6b80370ce10cd5bde8977c1bc11100afb4
Duncan <duncanmarten@yahoo.com>
2020-11-11 10:04:21 -0600

    added routes for accessing api and html data

---
+++ Develop/routes/apiRoutes/index.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const { createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    
+    res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on next index of array
+    req.body.id = notes.length.toString();
+
+    // if any data is incorrect, send 400 error
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+
+        res.json(note);
+    }
+});
+
+
+module.exports = router;
